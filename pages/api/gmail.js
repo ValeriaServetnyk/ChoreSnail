492b73aae2432f0630d8ff0cc64f96f8568d7510
ValeriaServetnyk <102434294+ValeriaServetnyk@users.noreply.github.com>
2022-07-14 12:54:14 +0200

    Add gmail endpoint to send chore lists to participantAdd gmail endpoint to send chore lists to participantss

---
+++ pages/api/gmail.js
@@ -0,0 +1,56 @@
+import { config } from 'dotenv-safe';
+import { google } from 'googleapis';
+import { request } from 'http';
+import nodemailer from 'nodemailer';
+
+config();
+
+export default function createEmailHandler(req, res) {
+  if (req.method === 'POST') {
+    if (
+      typeof request.body.participantName !== 'string' ||
+      !request.body.participantName ||
+      typeof request.body.participantEmail !== 'string' ||
+      !request.body.participantEmail
+    ) {
+      res.status(400).json({ errors: [{ message: 'Invalid name or email' }] });
+      return;
+    }
+    const clientId = process.env.CLIENT_ID;
+    const clientSecret = process.env.CLIENT_SECRET;
+    const redirectUrl = process.env.REDIRECT_URL;
+    const refreshToken = process.env.REFRESH_TOKEN;
+
+    const myOAuth2Client = new google.auth.OAuth2(
+      clientId,
+      clientSecret,
+      redirectUrl,
+    );
+
+    myOAuth2Client.setCredentials({ refresh_token: refreshToken });
+
+    const myAccessToken = myOAuth2Client.getAccessToken();
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: 'choresnail@gmail.com',
+        clientId: clientId,
+        clientSecret: clientSecret,
+        refreshToken: refreshToken,
+        accessToken: myAccessToken,
+      },
+    });
+    const mailData = transporter.sendMail({
+      from: 'Choresnail <choresnail@gmail.com>',
+      to: 'Leraservetnik@gmail.com',
+      // request.body.email,
+      subject: 'Sending Email using Node.js',
+      text: 'That was easy!',
+    });
+    response.status(200).json({ mailData: mailData });
+    return;
+  }
+  res.status(405).json({ errors: [{ message: 'Method not allowed' }] });
+}
